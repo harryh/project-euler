11e153b5a24315e078fd164536dfd198264899d1
Harry Heymann <harryh@gmail.com>
2016-05-23 14:43:20 -0400

    Simplify 008

--- 008.scala
+++ 008.scala
@@ -1,7 +1,4 @@
-val digits = scala.io.Source.fromFile("008.txt")
-                            .getLines()
-                            .flatMap(_.toList.map(d => (d - '0').toLong))
-                            .toVector
-val n = 13
-val options = for (i <- 0 to digits.length - n) yield digits.slice(i, i + n).reduceLeft(_*_)
-options.max
+inputFile("008.txt").flatMap(_.map(_.asDigit.toLong))
+                    .sliding(13)
+                    .map(_.product)
+                    .max
