1df0e99fd1cabb32ad2abd66c601aee0edaf2fae
Harry Heymann <harryh@gmail.com>
2016-05-26 11:47:50 -0400

    Simplify 059

--- 059.scala
+++ 059.scala
@@ -1,17 +1,13 @@
 val cipherText = inputFile("059.txt").flatMap(_.split(",")).map(_.toInt.toChar).mkString
 
-val keys = for {
-  a <- 'a' to 'z'
-  b <- 'a' to 'z'
-  c <- 'a' to 'z'
-} yield List(a, b, c)
-
-def decode(cipherText: String, key: List[Char]): String = {
+def decode(key: Char*): String = {
   val keys = Stream.continually(key).flatten
   cipherText.zip(keys).map(t => (t._1 ^ t._2).toChar).mkString
 }
 
-keys.map(key => decode(cipherText, key))
-    .maxBy(_.count(_.isLetter))
-    .map(_.toInt)
-    .sum
+val letters = 'a' to 'z'
+val plainTexts = for (a <- letters; b <- letters; c <- letters) yield decode(a, b, c)
+
+plainTexts.maxBy(_.count(_.isLetter))
+          .map(_.toInt)
+          .sum
