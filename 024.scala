c2d2147b401a4221f9fea738ab81f05906c98005
Harry Heymann <harryh@gmail.com>
2016-06-06 09:32:06 -0400

    024

--- 024.scala
+++ 024.scala
@@ -1,5 +1,10 @@
-def nextLexographicPermutation(from: String): Option[String] = {
-  Some(from)
-}
+var digits = (0 to 9).toList
+var remain = BigInt(1000000 - 1)
 
-iteratedStream("0123456789".sorted)(nextLexographicPermutation)(1000000 - 1)
+(for (i <- 1 to digits.length) yield {
+  val j = remain / factorial(10 - i)
+  remain = remain % factorial(10 - i)
+  val (a, b) = digits.splitAt(j.toInt)
+  digits = a ++ b.tail
+  b.head
+}).mkString
