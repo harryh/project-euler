31be18ad8d8cf318db957b4c7d42a86e4121314e
Harry Heymann <harryh@gmail.com>
2016-06-06 12:21:44 -0400

    Simplify 024

--- 024.scala
+++ 024.scala
@@ -1,12 +1,12 @@
-var digits = (0 to 9).toList
+var digits = "0123456789"
 var remain = 1000000 - 1
 
 for (i <- 1 to digits.length) {
   val fact = factorial(10 - i).toInt
   val split = remain / fact
   remain = remain % fact
-  val (a, b::c) = digits.splitAt(split)
-  digits = a ++ c :+ b
+  val (a, b) = digits.splitAt(split)
+  digits = a ++ b.tail :+ b.head
 }
 
-digits.mkString
+digits
