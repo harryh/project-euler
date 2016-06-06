21d8fa2e7ec767ad09ba642229c769e3142c0e28
Harry Heymann <harryh@gmail.com>
2016-06-06 15:11:12 -0400

    Inline function for 024

--- 024.scala
+++ 024.scala
@@ -1,10 +1,6 @@
-val digits = "0123456789"
-
-def pickDigit(tuple: (String, Int), i: Int): (String, Int) = {
+(1 to 10).foldLeft(("0123456789", 1000000 - 1))((tuple: (String, Int), i: Int) => {
   val (digits, remain) = tuple
   val fact = factorial(digits.length - i).toInt
   val (a, b) = digits.splitAt(remain / fact)
   (a + b.tail + b.head, remain % fact)
-}
-
-(1 to digits.length).foldLeft((digits, 1000000 - 1))(pickDigit)._1
+})._1
