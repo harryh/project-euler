3dcc47cc40dc441e08569a06342640699aedc687
Harry Heymann <harryh@gmail.com>
2016-06-06 15:14:00 -0400

    Remove unncessary type annotations from 024

--- 024.scala
+++ 024.scala
@@ -1,4 +1,4 @@
-(1 to 10).foldLeft(("0123456789", 1000000 - 1))((tuple: (String, Int), i: Int) => {
+(1 to 10).foldLeft(("0123456789", 1000000 - 1))((tuple, i) => {
   val (digits, remain) = tuple
   val fact = factorial(digits.length - i).toInt
   val (a, b) = digits.splitAt(remain / fact)
